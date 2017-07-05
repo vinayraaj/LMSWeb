89804acaed5f8a897c1b023ff022f369a360f23e
vinayraaj <vinz4ever007@gmail.com>
2017-07-06 02:09:14 +0530

    Add files via upload

---
+++ title_index.php
@@ -0,0 +1,38 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/L.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc">JYOTHY INSTITUTE OF TECHNOLOGY</p>
+							<h3>
+
+							<p>Learning management system</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>JIT EXCELS</p>
+												<p>Excellence, Competence and Educational</p>
+												<p>Leadership in Science and Technology</p>
+								</div>		
+						</div>		
+				</div>
